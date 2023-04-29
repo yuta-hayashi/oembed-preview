acbc10f96151f50f4e9ce5382e120d38dbe2c335
yuta-hayashi <dev.hyuta@gmail.com>
2023-04-29 23:47:07 +0900

    Add Cloudflare Web Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Cloudflare Web Analytics */}
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "cec8516543f64a34989becea2c9043ac"}'
+        />
       </body>
     </Html>
   )
